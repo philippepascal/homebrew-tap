7a13561b32249a56796f488969b6b24c012697fb
Philippe Pascal <philippepascal@users.noreply.github.com>
2026-05-30 00:30:29 -0700

    Update apm formula to 0.1.43

--- Formula/apm.rb
+++ Formula/apm.rb
@@ -1,13 +1,13 @@
 class Apm < Formula
   desc "Agentic project manager — CLI and server"
   homepage "https://github.com/philippepascal/apm"
-  version "0.1.42"
+  version "0.1.43"
   license "BSL-1.1"
 
   on_macos do
     on_arm do
       url "https://github.com/philippepascal/apm/releases/download/v#{version}/apm-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "2acbde815780aa597b4aa42720c62af2878f3505c8b7bb023b40bbec8b965cee"
+      sha256 "a279ef18ca162556faaf4bb34f2110d965fc61ed55f3dc9f87abcab853d6b577"
     end
   end
 
