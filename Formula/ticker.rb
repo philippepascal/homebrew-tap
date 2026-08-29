c36da9138a6ad4025ea05955e168e43438bfaa26
philippepascal <philippe@pascalonline.org>
2026-08-28 18:34:38 -0700

    ticker v0.1.1

--- Formula/ticker.rb
+++ Formula/ticker.rb
@@ -1,13 +1,13 @@
 class Ticker < Formula
   desc "Tick-based spreadsheet for time series, finance, and simulation"
   homepage "https://github.com/philippepascal/ticker"
-  version "0.1.0"
+  version "0.1.1"
   license "BUSL-1.1"
 
   on_macos do
     on_arm do
       url "https://github.com/philippepascal/ticker-releases/releases/download/v#{version}/ticker-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "ab426d7fd459c4c774dcb90d31846754cc0c91a3d42965f4b79d53d054306f6b"
+      sha256 "adff92770de1ee3513be0f89839e7e91380585c400a10c8803cc9a58948917cf"
     end
   end
 
